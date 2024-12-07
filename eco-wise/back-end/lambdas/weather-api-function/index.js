357a80103b1095cbd80d4765b1666c8a75f56f8f
brian <brianyuk@gmail.com>
2024-12-07 13:58:54 +0800

    Add table operations for weather api function

--- eco-wise/back-end/lambdas/weather-api-function/index.js
+++ eco-wise/back-end/lambdas/weather-api-function/index.js
@@ -2,30 +2,30 @@ const axios = require('axios').default;
 const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-const tableName = '<YourTableName>';
+const tableName = 'WeatherTable';
 
 // Function to insert data into DynamoDB
 const insertDataIntoDynamoDB = async (transformedData) => {
-  // try {
-  //   for (const item of transformedData) {
-  //     const params = {
-  //       TableName: tableName,
-  //       Item: {
-  //         name: item.name,
-  //         latitude: item.label_location.latitude,
-  //         longitude: item.label_location.longitude,
-  //         forecast: item.forecast,
-  //         timestamp: new Date().toISOString(), // Optional: Add a timestamp
-  //       },
-  //     };
+  try {
+    for (const item of transformedData) {
+      const params = {
+        TableName: tableName,
+        Item: {
+          name: item.name,
+          latitude: item.label_location.latitude,
+          longitude: item.label_location.longitude,
+          forecast: item.forecast,
+          timestamp: new Date().toISOString(), // Optional: Add a timestamp
+        },
+      };
 
-  //     await dynamoDB.put(params).promise();
-  //     console.log(`Inserted item into DynamoDB: ${JSON.stringify(params.Item)}`);
-  //   }
-  // } catch (error) {
-  //   console.error('Error inserting data into DynamoDB:', error);
-  //   throw new Error('Failed to insert data into DynamoDB.');
-  // }
+      await dynamoDB.put(params).promise();
+      console.log(`Inserted item into DynamoDB: ${JSON.stringify(params.Item)}`);
+    }
+  } catch (error) {
+    console.error('Error inserting data into DynamoDB:', error);
+    throw new Error('Failed to insert data into DynamoDB.');
+  }
   console.log('test')
 };
 
