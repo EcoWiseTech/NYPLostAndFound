063016d6b607a42dcbea7f9bc8d96d92abf26bef
brian <brianyuk@gmail.com>
2024-12-12 17:54:58 +0800

    User profile picture UI init

--- eco-wise/front-end/src/components/common/Navbar/ProfilePicture.jsx
+++ eco-wise/front-end/src/components/common/Navbar/ProfilePicture.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Avatar } from '@mui/material'
+import md5 from "md5";
+import { stringAvatar } from '../../../functions/StringAvatar';
+import { useUserContext } from '../../../contexts/UserContext';
 
-function ProfilePicture() {
+function ProfilePicture(props) {
+  const { user } = useUserContext();
+  const email_md5 = md5(user.UserAttributes.email)
+  const s = {
+    ...stringAvatar(user.UserAttributes.given_name).sx,
+    ...props.sx
+  }
   return (
-    <div>ProfilePicture</div>
+    <>
+      {/* {user.profile_picture_type === "gravatar" && <Avatar {...props} src={"https://www.gravatar.com/avatar/" + email_md5} />}
+      {user.profile_picture_type === "local" && <Avatar {...props} src={user.profile_picture + "?t=" + new Date().getTime()} />} */}
+      {!user.profile_picture_type && <Avatar  {...stringAvatar(user.UserAttributes.given_name)} sx={s} />}
+    </>
   )
 }
 
