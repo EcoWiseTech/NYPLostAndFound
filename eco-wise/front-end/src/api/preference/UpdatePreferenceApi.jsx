14f11fdc02751968f5a242c3401a7d24d83a80bc
frankruszel <110735562+frankruszel@users.noreply.github.com>
2025-01-26 09:37:05 +0800

    add CRUD api function for preference

---
+++ eco-wise/front-end/src/api/preference/UpdatePreferenceApi.jsx
@@ -0,0 +1,17 @@
+import EcoWiseApi from "../APIRequest";
+
+export const UpdatePreferenceApi = async (requestBody) => {
+    console.log('reahced', requestBody)
+    try {
+  
+      // Make the POST request using the APIRequest class
+      const response = await EcoWiseApi.put('/Preference/UpdatePreference', requestBody);
+  
+      // Return the successful response
+      return response.data;
+    } catch (error) {
+      console.error('Error Updating Preference:', error);
+      // Re-throw the error for higher-level handling
+      throw error;
+    }
+  };
