1dd477cbf1bc152ae128e5653206928681196b71
frankruszel <110735562+frankruszel@users.noreply.github.com>
2025-01-26 23:09:04 +0800

    fix CORS bug for GetGSIDeviceCsonumption

--- eco-wise/back-end/SAM/GetGSIDeviceConsumptionApiFunction/app.mjs
+++ eco-wise/back-end/SAM/GetGSIDeviceConsumptionApiFunction/app.mjs
@@ -2,23 +2,19 @@ import AWS from 'aws-sdk';
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const tableName = 'DeviceConsumptionTable';
+const indexName = 'userId-startTime-index';
 
 // Function to query device consumption data from DynamoDB
-const queryDeviceConsumptionFromDynamoDB = async (userId, startTime) => {
+const queryDeviceConsumptionFromDynamoDB = async (userId,startTime) => {
   try {
     let params = {
       TableName: tableName,
+      IndexName: indexName,
       KeyConditionExpression: 'userId = :userId ', // Query by userId
       ExpressionAttributeValues: {
         ':userId': userId,
       },
     };
-
-    // If startTime is provided, add it to the query
-    if (startTime) {
-      params.KeyConditionExpression += ' AND startTime = :startTime';
-      params.ExpressionAttributeValues[':startTime'] = startTime;
-    }
 
     const result = await dynamoDB.query(params).promise();
     console.log(`Queried device consumption data for userId: ${userId} with startTime: ${startTime}`);
@@ -37,6 +33,7 @@ export const lambdaHandler = async (event, context) => {
     // Extract userId and startTime from query parameters
     const userId = event.queryStringParameters && event.queryStringParameters.userId;
     const startTime = event.queryStringParameters && event.queryStringParameters.startTime;
+    console.log(`HERE userid: userId`)
 
     if (!userId) {
       return {
@@ -53,7 +50,7 @@ export const lambdaHandler = async (event, context) => {
     }
 
     // Query DynamoDB for device consumption data with or without startTime
-    const consumptionData = await queryDeviceConsumptionFromDynamoDB(userId, startTime);
+    const consumptionData = await queryDeviceConsumptionFromDynamoDB(userId,null);
 
     // Ensure that at least one item is returned
     if (consumptionData.length === 0) {
@@ -86,6 +83,11 @@ export const lambdaHandler = async (event, context) => {
     console.error('Error processing request:', error);
     return {
       statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*", 
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization", 
+      },
       body: JSON.stringify({
         message: 'An error occurred while processing the request.',
         error: error.message,
