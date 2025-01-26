ce9f87496cbb23ff2d078bbb284536b1514bffc1
frankruszel <110735562+frankruszel@users.noreply.github.com>
2025-01-26 12:49:50 +0800

    add import to fix displaty bug

--- eco-wise/front-end/src/pages/Budget.jsx
+++ eco-wise/front-end/src/pages/Budget.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState, useContext, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom'
-import { Box, Paper, Divider, Typography, Grid, Card } from '@mui/material';
+import { Box, Paper, Divider, Typography, Grid, Card, CircularProgress } from '@mui/material';
 import { AccessTime } from '@mui/icons-material';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import { jwtDecode } from 'jwt-decode';
 import { GetPreferenceApi } from '../api/preference/GetPreferenceApi';
 import { useUserContext } from '../contexts/UserContext';
 import { enqueueSnackbar } from 'notistack';
+
 
 
 function Budget() {
